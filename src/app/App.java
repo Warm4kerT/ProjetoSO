56759925b24cd290a464f81d9f9022ba7e504eea
Warm4kerT <lalmeida186@gmail.com>
2020-05-12 22:37:50 +0100

    commit

--- src/app/App.java
+++ src/app/App.java
@@ -3,13 +3,6 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception { 
-        ArrayList <PCB> processos=fileRW.readF("./processos.txt");
-        System.out.println("insira o processo a ler:\n");
-        PCB aux=new PCB(read.aInt(),read.aInt(),read.aInt(),read.aInt(),read.aInt(),read.aInt(),read.aString());
-        processos.add(aux);
-        fileRW.writeF("./processos.txt", processos);
-
-        ArrayList <PCB> pro=fileRW.readF("./processos.txt");
-        fileRW.printList(pro);     
+       
     }
 }
