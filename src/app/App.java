df8ce1280121d70e759ae5e251d7b5bbff379aea
Warm4kerT <lalmeida186@gmail.com>
2020-05-13 00:01:11 +0100

    commit

--- src/app/App.java
+++ src/app/App.java
@@ -2,7 +2,20 @@ package app;
 import java.util.*;
 
 public class App {
-    public static void main(String[] args) throws Exception { 
+    public int tempo=0;
+    public int PC=0;
+
+    public static void main(String[] args){ 
+        ArrayList<PCB> PcbTabela=new ArrayList<>();
+        PcbTabela=(ArrayList<PCB>) fileRW.readF("input/plan.txt");
+        ArrayList<Memory> memo=new ArrayList<>();
+        for(PCB aux:PcbTabela){
+            System.out.println(aux.toString());
+            memo=(ArrayList<Memory>) fileRW.readF("input/"+aux.getNome()+".prg");
+            for(Memory aux1:memo){
+                System.out.println(aux1.toString());
+            }
+        }
        
     }
 }
--- src/app/fileRW.java
+++ src/app/fileRW.java
@@ -2,24 +2,44 @@ package app;
 import java.io.*;
 import java.util.*;
 
+
 public class fileRW {
 
     public static ArrayList<?> readF(String path){
         ArrayList<Memory> mList=new ArrayList<>();
-
+        ArrayList<PCB> pList=new ArrayList<>();
+        
         try{
             BufferedReader buff= new BufferedReader(new FileReader(path));
             String line;
             while((line=buff.readLine())!=null){
-                if(!line.equals("\n") && !line.equals("") && !line.equals(" ")){
+                if(!line.equals("\n") && !line.equals(" ")){
                     if(line.contains(" ")){
                         String[] parts=line.split(" ");
                         Memory aux=new Memory(parts[0], new Integer(parts[1]), parts[2]);
                         mList.add(aux);
+                    }else{
+                        if(line.contains("  ")){
+                            String[] parts=line.split("  ");
+                            PCB aux=new PCB();
+                            aux.setNome(parts[0]);
+                            aux.setStart(new Integer(parts[1]));
+                            pList.add(aux);
+                            
+                        }
                     }
                 }
             }
-            return mList;
+            if(buff!=null){
+                buff.close();;
+            }
+
+            if(!mList.isEmpty()){
+                return mList;
+            }else{
+                return pList;
+            }
+
         }catch(IOException e){
             System.out.println(e.getMessage());
 
